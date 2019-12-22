2ab74a2fedcd67142bd3f5a6e09d53bcd2224f56
刘宸 <strangejugg@hotmail.com>
2019-12-22 18:53:36 +0800

    UPD: FIRST VERSION

--- show.py
+++ show.py
@@ -6,9 +6,9 @@ from torchvision import transforms
 
 
 def pred():
-    img_id = 10
+    img_id = 35
     img = cv2.imread(f"dataset/test/{img_id:04}.jpg")
-    f = open("result/submission_change_kernel_size.csv")
+    f = open("result/submission.csv")
     points = []
     values = f.readlines()[img_id + 1].split(",")
     for i in range(98):
@@ -24,18 +24,18 @@ def eval():
     load_path = path.join(path.dirname(__file__), "model", "model_change_kernel_size.pth")
     net = torch.load(load_path)
     net.eval()
-    img_id = 5
+    img_id = 9
     img = cv2.imread(f"dataset/train/{img_id:04}.jpg")
-    img_new, ratio_H, ratio_W = rescale(img, (200, 200))
+    img_new, ratio = rescale(img, (200, 200))
 
-    img_new = cv2.cvtColor(img_new, cv2.COLOR_BGR2GRAY)[:, :, None]
+    # img_new = cv2.cvtColor(img_new, cv2.COLOR_BGR2GRAY)[:, :, None]
     print(img_new.shape)
     with torch.no_grad():
-        outputs = net(transforms.ToTensor()(img_new).view(-1, 1, 200, 200))
+        outputs = net(transforms.ToTensor()(img_new).view(-1, 3, 200, 200))
 
     points = []
     for i in range(98):
-        points.append((int(float(outputs[0][2 * i]/ratio_W)), int(float(outputs[0][2 * i + 1]/ratio_H))))
+        points.append((int(float(outputs[0][2 * i]/ratio)), int(float(outputs[0][2 * i + 1]/ratio))))
     for p in points:
         cv2.circle(img, p, 1, (0, 255, 0), 2)
 
